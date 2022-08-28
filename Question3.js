3a03c7a2b5d087d6c354dcd54bc5e63141570635
Jothi Krishna <56432134+JOTHIKRISHNA0109@users.noreply.github.com>
2022-08-28 09:13:15 +0530

    Create Question3.js

---
+++ Question3.js
@@ -0,0 +1,41 @@
+/*3. Write a program to find whether a given number is special number or
+not-
+If the sum of the factorial of digits of a number (N) is equal to the
+number itself, the number (N) is called a special number.
+eg- 145 is a special number
+Logic- 1! + 4! + 5!= 1+24+120 i.e 14*/
+
+
+function factorial(number) {
+  let factorial = 1;
+  for (let i=1;i<=number;i++) {
+      factorial*=i;
+  }
+  return factorial;
+}
+
+function isSpecial(number) {
+    let sum = 0;
+    let temp = number;
+    while(number>0) {
+        let remainder = Math.floor(number%10);
+        sum += factorial(remainder);
+        number = Math.floor(number/10);
+    }
+    if (temp == sum) {
+        return true;
+    }
+    return false;
+}
+
+function main() {
+    let number = 145; //input value
+    if (isSpecial(number)){
+        console.log("Yeahhh!!");
+    }
+    else {
+        console.log("Nahh!!");
+    }
+}
+
+main()
